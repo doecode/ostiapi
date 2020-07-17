d5ab911815a92e3148572dc5ceae2bb939bc6508
David Brown <dmlb2000@gmail.com>
2020-07-17 11:26:14 -0700

    Add setuptools support
    This adds a setup.py to include support for installing the ostiapi
    with pip. The ostiapi module is created as a directory with
    __init__.py at the root.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Setup and install the OSTI API client library."""
+from os import path
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ostiapi',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='OSTI Client API',
+    url='https://github.com/doecode/ostiapi/',
+    long_description=open(path.join(
+        path.abspath(path.dirname(__file__)),
+        'README.md')).read(),
+    long_description_content_type='text/markdown',
+    author='Neal Ensor',
+    author_email='EnsorN@osti.gov',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'dicttoxml'
+    ]
+)
