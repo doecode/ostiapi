780ccc6cbba841775d28ab1033604437a825e63a
David Brown <dmlb2000@gmail.com>
2020-07-21 13:53:02 -0700

    This refines some testing and adds actions
    This refines the testing and includes Github Actions as a CI/CD
    pipeline.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- tests/ostiapi_test.py
+++ tests/ostiapi_test.py
@@ -11,6 +11,7 @@ class TestOstiApi(TestCase):
 
     username = getenv('OSTI_USERNAME', 'my-osti-account')
     password = getenv('OSTI_PASSWORD', 'my-osti-password')
+    site_input_code = getenv('OSTI_SITE_CODE', 'my-osti-site-code')
 
     def setup_method(self, *_args, **kwargs):
         """Put the module into testing mode."""
@@ -22,7 +23,8 @@ class TestOstiApi(TestCase):
         return ostiapi.reserve(
             {
                 'title':'My upcoming dataset',
-                'accession_num':'sample-ds-0001'
+                'accession_num':'sample-ds-0001',
+                'site_input_code': self.site_input_code
             },
             self.username,
             self.password
@@ -40,14 +42,14 @@ class TestOstiApi(TestCase):
 
     def test_get(self):
         """Test the get method in the ostiapi module."""
+        reserve_result = self._setup_reserve()
         data = ostiapi.get(
-            self._setup_reserve()['record']['osti_id'],
+            reserve_result['record']['osti_id'],
             self.username,
             self.password
         )
         self.assertTrue('record' in data)
         self.assertTrue('title' in data['record'])
         self.assertEqual(data['record']['title'], 'My upcoming dataset')
-        self.assertEqual(data['record']['status'], 'SUCCESS')
-        self.assertEqual(data['record']['doi_status'], 'RESERVED')
-        self.assertFalse(data['record']['status_message'], 'Status message should be none')
+        self.assertEqual(data['record']['doi_status'], 'PENDING')
+        self.assertEqual(data['record']['accession_num'], 'sample-ds-0001')
