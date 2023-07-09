e718b51d2dca213ee748d880a2bcae8ebea693db
Montana Mendy <montana@linux.com>
2023-07-08 21:56:04 -0700

    Refactor for current standards.

--- setup.py
+++ setup.py
@@ -6,20 +6,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='ostiapi',
+    name="ostiapi",
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    description='OSTI Client API',
-    url='https://github.com/doecode/ostiapi/',
-    long_description=open(path.join(
-        path.abspath(path.dirname(__file__)),
-        'README.md')).read(),
-    long_description_content_type='text/markdown',
-    author='Neal Ensor',
-    author_email='EnsorN@osti.gov',
-    packages=find_packages(include=['ostiapi']),
-    install_requires=[
-        'requests',
-        'dicttoxml'
-    ]
+    setup_requires=["setuptools_scm"],
+    description="OSTI Client API",
+    url="https://github.com/doecode/ostiapi/",
+    long_description=open(
+        path.join(path.abspath(path.dirname(__file__)), "README.md")
+    ).read(),
+    long_description_content_type="text/markdown",
+    author="Neal Ensor",
+    author_email="EnsorN@osti.gov",
+    packages=find_packages(include=["ostiapi"]),
+    install_requires=["requests", "dicttoxml"],
 )
